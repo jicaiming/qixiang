db81a1c4aee200c6cc31d3bb0dc0b8f017f659e6
jicaiming <1450882835@qq.com>
2019-10-07 17:18:50 +0800

    项目环境搭建

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router , Route } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+
+// import { MapProvider } from './context/MapContext'
+import store from './store/'
+
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/index/message/message.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+export default class Message extends Component {
+    render(){
+        return (
+            <h1>message</h1>
+        )
+    }
+}
