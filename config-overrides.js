0e765ae028c8204de71dffa2b996e00f3c7b0058
guangruixiao <2357046105@qq.com>
2019-10-10 15:58:20 +0800

    2

--- config-overrides.js
+++ config-overrides.js
@@ -9,9 +9,9 @@ module.exports = override(
 
   addWebpackAlias({
     ['assets']: path.resolve(__dirname, 'src/assets'),
-    // ['styles']: path.resolve(__dirname, 'src/assets/styles'),
+    ['styles']: path.resolve(__dirname, 'src/assets/styles'),
     ['images']: path.resolve(__dirname, 'src/assets/images'),
-    // ['components']: path.resolve(__dirname, 'src/components'),
-    // ['utils']: path.resolve(__dirname, 'src/utils'),
+    ['components']: path.resolve(__dirname, 'src/components'),
+    ['utils']: path.resolve(__dirname, 'src/utils'),
   })
 )
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,20 +1,59 @@
 import React, { Component } from 'react'
 
-import { ProfileContainer } from './StyleProfile.js'
-import userImg from 'images/20150224120123_fUzMf.png'
+import { ProfileMessage, ProfileOrder, ProfileServe,ListItem } from './StyleProfile.js'
+import userImg from 'images/ui切图/20150224120123_fUzMf.png'
+import bianJi from 'images/ui切图/bianji.png'
+
+// import x from 'images/ui切图/bianji.png'
+// import g from 'images/ui切图/bianji.png'
+// import r from 'images/ui切图/bianji.png'
+// import c from 'images/ui切图/bianji.png'
+
+
+
 
 
 export default class Profile extends Component {
     render() {
         return (
-            <ProfileContainer>
-                <div>
+            <>
+                <ProfileMessage>
                     <div>
-                        <img src={userImg} alt="" />
+                        <div>
+                            <img src={userImg} alt="" />
+                        </div>
+                        <div>
+                            <img src={bianJi} alt="" />
+                            <div>
+                                <p>guangruixiao</p>
+                                <p>请添加活动区域</p>
+                                <p>请添加公司信息</p>
+                            </div>
+                        </div>
                     </div>
-
-                </div>
-            </ProfileContainer>
+                </ProfileMessage>
+                <ProfileOrder>
+                    <h4>我的订单</h4>
+                    <div>
+                        <ul>
+                            <li>
+                                <img src="" alt="" />
+                            </li>
+                            <li>haha</li>
+                            <li>sssssssss</li>
+                            <li>xc</li>
+                        </ul>
+                    </div>
+                </ProfileOrder>
+                <ProfileServe>
+                    <h4>我的服务</h4>
+                    <div>
+                        <ListItem><p>关于企业</p></ListItem>
+                        <ListItem><p>公告</p></ListItem>
+                        <ListItem><p>设置</p></ListItem>
+                    </div>
+                </ProfileServe>
+            </>
         )
     }
 }
