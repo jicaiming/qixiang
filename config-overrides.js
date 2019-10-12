803d9e1c83e9d5ac0ba682be81835c6db433eb47
jicaiming <1450882835@qq.com>
2019-10-12 11:47:56 +0800

    jjj

--- config-overrides.js
+++ config-overrides.js
@@ -11,18 +11,6 @@ module.exports = override(
     ['assets']: path.resolve(__dirname, 'src/assets'),
     ['styles']: path.resolve(__dirname, 'src/assets/styles'),
     ['images']: path.resolve(__dirname, 'src/assets/images'),
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    ['styles']: path.resolve(__dirname, 'src/assets/styles'),
-    ['images']: path.resolve(__dirname, 'src/assets/images'),
-=======
-    // ['styles']: path.resolve(__dirname, 'src/assets/styles'),
-    // ['images']: path.resolve(__dirname, 'src/assets/images'),
->>>>>>> origin/xxp
->>>>>>> 6a6a7af8a3dc8f425e2c17f58dac4a974de43e69
-=======
->>>>>>> xxp
     ['components']: path.resolve(__dirname, 'src/components'),
     ['utils']: path.resolve(__dirname, 'src/utils'),
   })
