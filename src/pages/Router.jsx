84f0463a3728b6bce3a915ef2a8ebf7ce0de2e6c
qianguangrong <1187949904@qq.com>
2019-10-12 17:17:39 +0800

    登录与注册

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import { Route , Switch , Redirect  } from 'react-router-dom'
 
+import Signin from './signin/Index'
+
 import Index from './Index'
 import Page404 from './Page404'
 import City from './home/City'
@@ -16,7 +18,9 @@ export default class MyRouter extends Component {
                     {/* <Route path='/details' component={Details} ></Route> */}
 
                     <Route path='/index' component={Index} ></Route>
-                    <Redirect from='/' exact to='/index/home'></Redirect> 
+                    {/* <Redirect from='/' exact to='/index/home'></Redirect>  */}
+                    <Route path='/signin' component={Signin} ></Route>
+                    <Redirect from='/' exact to='/signin'></Redirect> 
                     <Route path='/city' component={City} exact></Route>
                     <Route path='/classic' component={Classic} exact></Route>
                     <Route path='/classic/:type' component={ClassicDetails} exact></Route>
