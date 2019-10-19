987375d7c83eae661bc7e0ac4b6862bc811c1e40
qianguangrong <1187949904@qq.com>
2019-10-19 13:09:09 +0800

    qgr22

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -4,9 +4,6 @@ import { Route , Switch , Redirect  } from 'react-router-dom'
 
 import Index from './Index'
 import Page404 from './Page404'
-<<<<<<< HEAD
-import Signin from './signin/Index'
-=======
 import City from './home/City'
 import Classic from './home/Classic'
 import ClassicDetails from './home/ClassicDetails'
@@ -21,7 +18,6 @@ import MyOrder from './profile/MyOrder'
 
 
 
->>>>>>> master
 
 export default class MyRouter extends Component {
     render() {
@@ -34,19 +30,13 @@ export default class MyRouter extends Component {
                     {/* <Route path='/order/:id' component={Order} exact></Route> */}
                     <Route path='/order/:type' component={Order} exact></Route>
                     <Route path='/timer' component={Timer}></Route>
-                    <Route path='/index' component={Index} ></Route>
-<<<<<<< HEAD
-                    {/* <Redirect from='/' exact to='/index/home'></Redirect>  */}
-                    <Route path='/signin' component={Signin} ></Route>
-                    <Redirect from='/' exact to='/signin'></Redirect> 
-=======
+                    <Route path='/index' componenst={Index} ></Route>
                     <Route path='/myorder' component={MyOrder}></Route>
                     <Route path='/city' component={City} exact></Route>
                     <Route path='/classic' component={Classic} exact></Route>
                     <Route path='/classic/:type' component={ClassicDetails} exact></Route>
                     <Route path='/edit' component={Edit} exact></Route>
                     <Redirect from='/' exact to='/index/home'></Redirect> 
->>>>>>> master
                     <Route component={Page404} ></Route>
                 </Switch>
             </>
